95c4e4d260c0fc5fa65b31f581a0d30a368f482c
li xiaolin <lixiaolin@camera360.com>
2019-02-26 19:44:53 +0800

    first commit

---
+++ pages/create.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">页面 - 提问</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			type: 'free'
+		};
+	},
+	onLoad(option) {
+		this.type = option.type;
+	},
+	onShow() {
+		uni.showToast({
+			title: this.type
+		});
+	}
+};
+</script>
+
+<style>
+.content {
+	text-align: center;
+	height: 400upx;
+	margin-top: 200upx;
+}
+</style>
---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		首页
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style>
+.content {
+	text-align: center;
+	height: 400upx;
+	margin-top: 200upx;
+}
+</style>

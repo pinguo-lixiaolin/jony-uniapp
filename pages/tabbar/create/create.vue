fe5591d51c9a57b18acc2b5443d12c0aabc4ec62
xiaolin li <xiaolinli@xiaolindeMacBook-Pro.local>
2019-04-23 23:49:31 +0800

    优化代码删除多余图片，更新代码编译

--- pages/tabbar/create/create.vue
+++ pages/tabbar/create/create.vue
@@ -8,20 +8,20 @@
 		/>
 		<view class="tabbar-box-wrap" :class="{ active: active }">
 			<view class="tabbar-box">
-				<view class="tabbar-box-item" @click="goToPage('/pages/create/create?type=free')">
+				<view class="tabbar-box-item" @click.stop="goToPage('/pages/create/create?type=free')">
 					<image class="box-image" src="../../../static/img/qa.png" mode="aspectFit" />
 					<text class="explain">免费相册</text>
 				</view>
 				<view
 					class="tabbar-box-item"
-					@click="goToPage('/pages/create/create?type=profesional')"
+					@click.stop="goToPage('/pages/create/create?type=profesional')"
 				>
 					<image class="box-image" src="../../../static/img/video.png" mode="aspectFit" />
 					<text class="explain">专业相册</text>
 				</view>
 				<view
 					class="tabbar-box-item"
-					@click="goToPage('/pages/create/create?type=article')"
+					@click.stop="goToPage('/pages/create/create?type=article')"
 				>
 					<image
 						class="box-image"

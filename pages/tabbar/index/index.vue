abfd088ef222bf2f7a1e0f0f268e2b2dad1d71cf
li xiaolin <lixiaolin@camera360.com>
2019-06-11 18:51:53 +0800

    提交

--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,15 +1,18 @@
 <template>
 	<view class="home">
 		<banners />
-		<view class="tab-nav">
-			<uni-segmented-control :current="current" :values="items" :style-type="styleType" :active-color="activeColor"
-			 @clickItem="onClickItem" />
-		</view>
-		<view class="content">
-			<view class="content-box" v-show="current === 0">
+		<view class="main">
+			<view class="tab-nav">
+				<uni-segmented-control :current="current" :values="items" :style-type="styleType" :active-color="activeColor"
+				 @clickItem="onClickItem" />
 			</view>
-			<view class="content-box" v-show="current === 1">
-
+			<view class="content">
+				<view class="content-box" v-show="current === 0">
+					3
+				</view>
+				<view class="content-box" v-show="current === 1">
+					2
+				</view>
 			</view>
 		</view>
 	</view>
@@ -46,24 +49,29 @@
 </script>
 
 <style lang="less">
-	.content {
+	.main {
 		display: flex;
-		width: 100%;
-		height: calc(100vh - 90upx - var(--status-bar-height));
-		background: #ffffff;
-		box-sizing: border-box;
-		position: relative;
+		flex-direction: column;
+		min-height: calc(100vh - 90upx - var(--status-bar-height));
 
-		&-box {
-			height: 100%;
-			position: absolute;
-			display: flex;
-			flex-direction: column;
+		.tab-nav {
 			left: 0;
 			right: 0;
-			bottom: 0;
-			top: 0;
-			width: 100%;
+			position: fixed;
+			background-color: rgba(255, 255, 255, .9);
+			top: calc(90upx + var(--status-bar-height));
 		}
+
+		.content {
+			display: flex;
+			width: 100%;
+			flex: 1;
+
+			&-box {
+				width: 100%;
+				height: 300upx;
+			}
+		}
+
 	}
 </style>
